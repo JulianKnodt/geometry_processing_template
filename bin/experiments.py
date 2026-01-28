2f0cd12a14dd6ff0f893ebd33c4387d4f282745b
julianknodt <julianknodt@gmail.com>
2026-01-27 21:45:06 -0800

    Update experiment file

--- bin/experiments.py
+++ bin/experiments.py
@@ -13,31 +13,33 @@ else:
 args = None
 
 abl_dir = "ablations"
+out_dir = "outputs"
+output_sub_dir = lambda sd: os.path.join(out_dir, sd)
 
 def run(
   src, dst, flags, out_dir=abl_dir, src_dir="data", bin=bin_file, eval=True,
-  missing_only=False,
+  missing_only=False, stage="run", input_flag="-i",
 ):
+  assert(bin != ...), "Must specify bin for now"
   def cb():
     nonlocal missing_only
-    if "run" not in args.stages: return []
+    if stage not in args.stages: return []
     if args.match_output is not None and args.match_output not in dst: return []
     if args.force: missing_only = False
     out_json = f"{out_dir}/{dst[:-4]}.json"
     if missing_only and os.path.exists(out_json) and not args.force:
-      print(f"Skipping {src} -> {dst}, results at {out_json} already exist")
+      print(f"[⏭️]: Skipping {src} -> {dst}, results at {out_json} already exist")
       return []
     out_file = f"{out_dir}/{dst}"
     if missing_only and (not eval) and os.path.exists(out_file):
-      print(f"Skipping {src} -> {out_file}, destination exists")
+      print(f"[⏭️]: Skipping {src} -> {out_file}, destination exists")
       return []
-    cmds = [
-      f"{bin_file} -i {src_dir}/{src} -o {out_dir}/{dst} {flags} --stat-file {out_json}",
-    ]
-    if eval:
+    cmds = []
+    if not args.eval_only:
+      cmds.append(f"{bin} {input_flag} {src_dir}/{src} -o {out_dir}/{dst} {flags} --stats {out_json}")
+    if eval and out_file.endswith(".obj"):
       cmds.append(
         f"{sys.executable} bin/hausdorff.py -o {src_dir}/{src} -n {out_file} --stat-file {out_json}"
-
       )
     return cmds
   return cb
@@ -93,6 +95,7 @@ def arguments():
   a.add_argument("--skip-to", default=None, choices=list(experiments.keys()), help="skip to this experiment")
   a.add_argument("--match-output", default=None, help="Only match render outputs with this")
   a.add_argument("--force", action="store_true", help="Run all commands even if marked missing only")
+  a.add_argument("--eval-only", action="store_true", help="Only run evaluation")
   return a.parse_args()
 
 args = arguments()
@@ -102,26 +105,24 @@ experiment_timestamps = {}
 
 exp_file = "experiment_log.json"
 
-if len(args.experiments) > 0:
-  assert(not os.system("cargo build --release"))
-
-for exp in args.experiments:
-  if args.skip_to is not None:
-    if args.skip_to == exp: args.skip_to = None
-    else: continue
+def run_experiment(exp):
+  global experiment_timestamps
   print("-================================================-")
   print(f"\tStarting {exp}")
   print("-================================================-")
-
   if os.path.exists(exp_file):
     with open(exp_file, "r") as f:
       experiment_timestamps = json.load(f)
 
   for cmd_list in experiments[exp]:
+    # run sub-experiment if it's a string
+    if type(cmd_list) == str:
+      run_experiment(cmd_list)
+      continue
+
     for cmd in cmd_list():
       assert(not os.system(cmd)), cmd
       if args.first_only: exit()
-
   print("-================================================-")
   print(f"\tFinished {exp}!")
   print("-================================================-")
@@ -134,3 +135,8 @@ for exp in args.experiments:
   with open(exp_file, "w") as f:
     json.dump(experiment_timestamps, f, indent=2)
 
+for exp in args.experiments:
+  if args.skip_to is not None:
+    if args.skip_to == exp: args.skip_to = None
+    else: continue
+  run_experiment(exp)
